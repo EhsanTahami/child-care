2184208e6c3be1b85efb66f1ccb45ea7922190ee
EhsanTahami <73498059+EhsanTahami@users.noreply.github.com>
2023-02-02 13:39:36 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function asideFunctionOpen() {
     var element = document.getElementById("side-nav");
     element.classList.add("open-aside-navbar");
     var element = document.getElementById("side-nav").style.transform ="translateX(0)";
+    var element = document.getElementById("side-nav").style.display ="block";
     var element = document.getElementById("side-nav").style.transition =" all 3s linear";
     // var element = document.getElementById ("body").style.background = "#221d1d5f";
     var element = document.getElementById("head-section").style.filter = "blur(2px)";
@@ -11,6 +12,7 @@ function asideFunctionOpen() {
 }
 function asideFunctionClose() {
     var element = document.getElementById("side-nav").style.transform ="translateX(-100%)";
+    var element = document.getElementById("side-nav").style.display ="none";
     // var element = document.getElementById("body").style.background="white";
     var element = document.getElementById("head-section").style.filter = "blur(0px)";
     // element.classList.add("aside-navbar");
